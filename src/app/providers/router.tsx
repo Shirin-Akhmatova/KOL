1b8b1f3e9cbbefabf4b52aae0e0839f155cf871f
Shirin <kamisatosaan>
2025-05-08 16:51:03 +0600

    added all structure

---
+++ src/app/providers/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../../widgets/Layout/Layout";
+import Home from "../../pages/Home/Home";
+import About from "../../pages/About/About";
+
+export const myRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+        ],
+    },
+]);
