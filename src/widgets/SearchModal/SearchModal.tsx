f8ea80a7cf0ad1440ecefe384a45bfdf564fb422
Shirin <kamisatosaan>
2025-05-14 03:40:02 +0600

    обновила header

---
+++ src/widgets/SearchModal/SearchModal.tsx
@@ -0,0 +1,11 @@
+import styles from "./SearchModal.module.scss";
+
+function SearchModal() {
+  return (
+    <div className={styles.searchModal}>
+      <div></div>
+    </div>
+  );
+}
+
+export default SearchModal;
