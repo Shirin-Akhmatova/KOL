b8ee20997c275ee7ab464877b92853b41fa210db
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-05-16 16:28:47 +0600

    payment-modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Carts from "../../widgets/Carts/Carts";
 import MapBtn from "@/shared/ui/buttons/selectBtn/MapBtn";
 import MapGeo from "@/widgets/Carts/MapGeo";
+import Payment from "@/widgets/payment/Payment";
 // import CartFilters from "./CartFilters/CartFilters";
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,7 @@ function Home() {
       {/* <CartFilters /> */}
       {isOpen ? <MapGeo /> : <Carts />}
       <MapBtn setIsOpen={setIsOpen} isOpen={isOpen} />
+      <Payment />
     </div>
   );
 }
