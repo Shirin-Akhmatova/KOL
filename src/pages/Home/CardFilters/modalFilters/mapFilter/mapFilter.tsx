a5657c7f672a3a3575efa7a85948bbf3ca5642f1
Dmitrii <to4ka180294@gmail.com>
2025-06-03 11:13:56 +0600

    add error boundaries

--- src/pages/Home/CardFilters/modalFilters/mapFilter/mapFilter.tsx
+++ src/pages/Home/CardFilters/modalFilters/mapFilter/mapFilter.tsx
@@ -1,7 +1,7 @@
-import { Map, Placemark, YMaps, ZoomControl } from "@pbe/react-yandex-maps";
+// import { Map, Placemark, YMaps, ZoomControl } from "@pbe/react-yandex-maps";
 import globalStyles from "../../cardFilters.module.scss";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import styles from "./mapFilter.module.scss";
+// import styles from "./mapFilter.module.scss";
 import useFilters from "@/shared/hooks/useFilters";
 
 interface IMapFilterProps {
@@ -61,29 +61,30 @@ function MapWithCoords() {
   }, []);
 
   return (
-    <YMaps query={{ apikey: import.meta.env.VITE_YANDEX_MAP_API }}>
-      <Map
-        className={styles.map}
-        defaultState={{ center: [41.97, 77.29], zoom: 7 }}
-        width="100%"
-        height="100%"
-        instanceRef={(ref) => {
-          if (ref) mapRef.current = ref;
-          setIsMapReady(true);
-        }}
-      >
-        <ZoomControl options={{ position: { right: 10, top: 10 } }} />
-        {getCoords && (
-          <Placemark
-            geometry={[getCoords.lat, getCoords.lng]}
-            options={{
-              preset: "islands#violetStretchyIcon",
-              iconColor: "#ff5a5f",
-            }}
-          />
-        )}
-      </Map>
-    </YMaps>
+    // <YMaps query={{ apikey: import.meta.env.VITE_YANDEX_MAP_API }}>
+    //   <Map
+    //     className={styles.map}
+    //     defaultState={{ center: [41.97, 77.29], zoom: 7 }}
+    //     width="100%"
+    //     height="100%"
+    //     instanceRef={(ref) => {
+    //       if (ref) mapRef.current = ref;
+    //       setIsMapReady(true);
+    //     }}
+    //   >
+    //     <ZoomControl options={{ position: { right: 10, top: 10 } }} />
+    //     {getCoords && (
+    //       <Placemark
+    //         geometry={[getCoords.lat, getCoords.lng]}
+    //         options={{
+    //           preset: "islands#violetStretchyIcon",
+    //           iconColor: "#ff5a5f",
+    //         }}
+    //       />
+    //     )}
+    //   </Map>
+    // </YMaps>
+    <div></div>
   );
 }
 
