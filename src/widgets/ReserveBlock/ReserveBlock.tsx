1ab01f784e5e6f22cb1211e116508647c34f7e6f
Shirin <kamisatosaan>
2025-06-10 18:24:40 +0600

    fixed reserve block

--- src/widgets/ReserveBlock/ReserveBlock.tsx
+++ src/widgets/ReserveBlock/ReserveBlock.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./style.module.scss";
+import { DateRange, type Range } from "react-date-range";
 import { format } from "date-fns";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
@@ -14,19 +15,6 @@ function ReserveBlock() {
       key: "selection",
     },
   ]);
-import Calendar from "../Calendar/CalendarUp";
-
-type DatePicker = {
-  startDate: Date | null;
-  endDate: Date | null;
-};
-
-function ReserveBlock() {
-  const [showPicker, setShowPicker] = useState<boolean>(false);
-  const [datePicker, setDatePicker] = useState<DatePicker>({
-    startDate: null,
-    endDate: null,
-  });
   const [isVisible, setIsVisible] = useState(false);
   const [isCompact, setIsCompact] = useState(false);
 
@@ -91,60 +79,6 @@ function ReserveBlock() {
               <div className={`${styles.compactInput} ${styles.bottom}`}>
                 <span className={styles.inputTitle}>GUESTS</span>
                 <span>1 guest</span>
-      <div className={styles.reserveOwerlay}>
-        <div className={styles.reserveHeader}>
-          <div className={styles.reservePriceBlock}>
-            <span className={styles.reservePrice}>$500</span>
-            <span className={styles.reserveDiscountPrice}>$440</span>
-            <span className={styles.reserveTime}>night</span>
-          </div>
-          <div className={styles.ratingBlock}>
-            <div className={styles.stars}>
-              <img src="/imgs/svgs/star.svg" alt="star" />
-              4.44
-            </div>
-            <div className={styles.reviewsDot}></div>
-            <div className={styles.reviews}>337 reviews</div>
-          </div>
-        </div>
-
-        <div>
-          <div className={styles.datePicker}>
-            <div
-              className={`${styles.datePickerInput} ${styles.input} ${styles.start}`}
-              onClick={() => setShowPicker(!showPicker)}
-            >
-              <span className={styles.inputTitle}>CHECK-IN</span>
-              {datePicker.startDate
-                ? format(datePicker.startDate, "dd/MM/yyyy")
-                : "Выберите день"}
-            </div>
-            <div
-              className={`${styles.datePickerInput} ${styles.input} ${styles.end}`}
-              onClick={() => setShowPicker(!showPicker)}
-            >
-              <span className={styles.inputTitle}>CHECKOUT</span>
-              {datePicker.endDate
-                ? format(datePicker.endDate, "dd/MM/yyyy")
-                : "Выберите день"}
-            </div>
-            {showPicker && (
-              <div
-                style={{
-                  position: "absolute",
-                  width: "100%",
-                  top: "100%",
-                  zIndex: 10,
-                }}
-              >
-                <Calendar
-                  values={datePicker}
-                  onChangeValue={setDatePicker}
-                  className={styles.reserveCalendar}
-                  onClose={() => {
-                    setShowPicker(false);
-                  }}
-                />
               </div>
               <div className={styles.compactPriceInfo}>
                 <span className={styles.compactNights}>5 nights</span>
@@ -161,7 +95,6 @@ function ReserveBlock() {
                 <span className={styles.reserveTime}>night</span>
               </div>
             </div>
-
             <div>
               <div className={styles.datePicker}>
                 <div
