7cf04cf7dc71ed7d0e17681b3b730f21fce3f519
Daniyar Kashkaraliev <daniyar.kashkaraliev@gmail.com>
2025-06-23 15:32:01 +0600

    add favorites page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,12 +4,13 @@ import Carts from "../../widgets/Carts/Carts";
 import MapBtn from "@/shared/ui/buttons/selectBtn/MapBtn";
 import MapGeo from "@/widgets/Carts/MapGeo";
 import CardFilters from "./CardFilters/CardFilters";
+import { blocks } from "@/widgets/mockData";
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <CardFilters />
-      {isOpen ? <MapGeo /> : <Carts />}
+      {isOpen ? <MapGeo /> : <Carts cardList={blocks} />}
       <MapBtn setIsOpen={setIsOpen} isOpen={isOpen} />
     </div>
   );
