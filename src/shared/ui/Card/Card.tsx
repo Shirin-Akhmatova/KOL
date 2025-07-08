a04f816e0eb1d506b20749bb85ca2cbe6bcf13c1
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-07-08 13:57:19 +0300

    Update Card.tsx

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import starIcon from "@/assets/icons/star.svg";
@@ -6,51 +5,76 @@ import like from "@/assets/icons/like.png";
 import notLike from "@/assets/icons/without like.png";
 import style from "./Card.module.scss";
 import type { Block } from "@/widgets/mockData";
+import { useEffect, useState } from "react";
 
 interface PropsType {
   block: Block | null;
-  addLike: (block: any) => void;
-  likes: Block[];
   mapModalBlock: boolean;
+  height?: string;
 }
 
-const Card = ({ block, addLike, likes, mapModalBlock }: PropsType) => {
+const Card = ({ block, mapModalBlock, height }: PropsType) => {
   if (!block) return null;
+
+  const [likes, setLikes] = useState<Block[]>([]);
+
+  const likesFromLocalStore = () => {
+    try {
+      const item = localStorage.getItem("favorites");
+      return item ? setLikes(JSON.parse(item)) : null;
+    } catch {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    likesFromLocalStore();
+  }, [addLike]);
+
+  function addLike(block: Block) {
+    const item = localStorage.getItem("favorites");
+    if (!item || !likes.find((el) => el.id === block.id)) {
+      localStorage.setItem("favorites", JSON.stringify([...likes, block]));
+    } else {
+      localStorage.setItem(
+        "favorites",
+        JSON.stringify(likes.filter((el) => el.id !== block.id))
+      );
+    }
+  }
   return (
     <div className={style.Card}>
-      <div className={style.images}>
-        <Swiper
-          modules={[Navigation, Pagination]}
-          pagination={{ clickable: true, dynamicBullets: true }}
-          spaceBetween={0}
-          slidesPerView={1}
-          className={style.swiper}
-        >
-          {block.images.map((image, imgIndex) => (
-            <SwiperSlide className={style.slide} key={imgIndex}>
-              <Link to={`/cardPage/${block.id}`}>
+      <a href={`/cardPage/${block.id}`}>
+        <div style={{ height: height }} className={style.images}>
+          <Swiper
+            modules={[Navigation, Pagination]}
+            pagination={{ clickable: true, dynamicBullets: true }}
+            spaceBetween={0}
+            slidesPerView={1}
+            className={style.swiper}
+          >
+            {block.images.map((image, imgIndex) => (
+              <SwiperSlide className={style.slide} key={imgIndex}>
                 <img src={image} alt="image" />
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        {likes.find((el) => el.id === block.id) ? (
-          <img
-            className={style.like}
-            src={like}
-            alt="like"
-            onClick={() => addLike(block)}
-          />
-        ) : (
-          <img
-            className={style.like}
-            src={notLike}
-            alt="notLike"
-            onClick={() => addLike(block)}
-          />
-        )}
-      </div>
-      <Link to={`/cardPage/${block.id}`}>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {likes.find((el) => el.id === block.id) ? (
+            <img
+              className={style.like}
+              src={like}
+              alt="like"
+              onClick={() => addLike(block)}
+            />
+          ) : (
+            <img
+              className={style.like}
+              src={notLike}
+              alt="notLike"
+              onClick={() => addLike(block)}
+            />
+          )}
+        </div>
         <div className={style.content}>
           <div className={style.info}>
             <h4>{block.title}</h4>
@@ -67,7 +91,7 @@ const Card = ({ block, addLike, likes, mapModalBlock }: PropsType) => {
             {mapModalBlock && <span>{block.data}</span>}
           </h4>
         </div>
-      </Link>
+      </a>
     </div>
   );
 };
